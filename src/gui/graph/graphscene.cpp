a3995d0890b5788260ca489d8a7e6f8fa8ab7c27
Sascha Peilicke <sasch.pe@gmx.de>
2010-01-18 19:39:14 +0100

    Better test/example code.

--- src/gui/graph/graphscene.cpp
+++ src/gui/graph/graphscene.cpp
@@ -98,9 +98,11 @@ void GraphScene::init()
     node2->setPos(0, 10);
     NodeItem *node3 = new NodeItem("3", NULL, this);
     node3->setPos(-100, 50);
-    DirectedEdgeItem *edge1 = new DirectedEdgeItem(node1, node2, "a", NULL, this);
-    DirectedEdgeItem *edge2 = new DirectedEdgeItem(node2, node3, "b", NULL, this);
-    ArrowItem *arrow1 = new ArrowItem(0, -100, 100, -50, "a1", NULL, this);
+
+    DirectedEdgeItem *edge1 = new DirectedEdgeItem(node1, node2, "12", NULL, this);
+    DirectedEdgeItem *edge2 = new DirectedEdgeItem(node2, node3, "23", NULL, this);
+
+    ArrowItem *a1 = new ArrowItem(0, 0, 0, -50, "a1", NULL, this);
 
     readSettings();
     emit graphChanged();
