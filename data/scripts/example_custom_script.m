9dc3184bd2ce4869db935901c34074be6f2ffdb3
Sascha Peilicke <sasch.pe@gmx.de>
2010-03-01 19:52:59 +0100

    Renamed 'dummy.m' to 'example_custom_script.m' to explain it's purpose and changed it accordingly.

--- data/scripts/example_custom_script.m
+++ data/scripts/example_custom_script.m
@@ -1,7 +1,7 @@
 % Dummy matlab script that does nothing except printing it's parameters
 
-function dummy(Eq,Num,Den)
-    fprintf('\n-- Dummy Matlab Script --\n')
+function example_custom_script(Eq,Num,Den)
+    fprintf('\n-- Example Custom Dummy Matlab Script --\n')
     fprintf('Eq  : %s\n',char(Eq));
     fprintf('Num : %s\n',char(Num));
     fprintf('Den : %s\n',char(Den));
