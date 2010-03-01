0022c59d0be4be9579704b9b51bc0069d856c51b
Sascha Peilicke <sasch.pe@gmx.de>
2010-03-01 19:35:18 +0100

    Added a simple 'dummy.m' Matlab script to demonstrate the custom script feature.

---
+++ data/scripts/dummy.m
@@ -0,0 +1,7 @@
+% Dummy matlab script that only
+function dummy(Eq,Num,Den)
+    fprintf('\n-- Dummy Matlab Script --\n')
+    fprintf('Eq  : %s\n',Eq);
+    fprintf('Num : %s\n',Num);
+    fprintf('Den : %s\n',Den);
+end
