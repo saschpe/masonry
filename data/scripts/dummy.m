170215dea4af2ed5427f6da8ed6d5b5fd513b9d5
Sascha Peilicke <sasch.pe@gmx.de>
2010-03-01 19:39:19 +0100

    Enhanced 'dummy.m' source code comment.

--- data/scripts/dummy.m
+++ data/scripts/dummy.m
@@ -1,4 +1,5 @@
-% Dummy matlab script that only
+% Dummy matlab script that does nothing except printing it's parameters
+
 function dummy(Eq,Num,Den)
     fprintf('\n-- Dummy Matlab Script --\n')
     fprintf('Eq  : %s\n',char(Eq));
