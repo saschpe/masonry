6032f67e55d5e11bb9784ca45e17e1764bceaa49
Sascha Peilicke <sasch.pe@gmx.de>
2010-03-01 19:38:27 +0100

    Fixed printing of equation of type 'sym'.

--- data/scripts/dummy.m
+++ data/scripts/dummy.m
@@ -1,7 +1,7 @@
 % Dummy matlab script that only
 function dummy(Eq,Num,Den)
     fprintf('\n-- Dummy Matlab Script --\n')
-    fprintf('Eq  : %s\n',Eq);
-    fprintf('Num : %s\n',Num);
-    fprintf('Den : %s\n',Den);
+    fprintf('Eq  : %s\n',char(Eq));
+    fprintf('Num : %s\n',char(Num));
+    fprintf('Den : %s\n',char(Den));
 end
